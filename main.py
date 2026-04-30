55e5f20d234f5d3e4b2299847606eef19d89a10b
mohansarath95 <mohansarath95@gmail.com>
2026-04-30 07:53:32 +0000

    Take user input as prompt

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,13 +14,14 @@ if not api_key:
     raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
 client = genai.Client(api_key=api_key)  # Set up the client with the API key
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+prompt = args.user_prompt               # Use the user prompt from the command line argument
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents=prompt
 )
 # Verify that usage_metadata is present
 if response.usage_metadata is None:
     raise RuntimeError("API request failed: usage_metadata is missing from the response.")
+
 print(f"User prompt: {prompt}")
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
